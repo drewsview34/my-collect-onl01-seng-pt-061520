6c040d97ebdd6029805720b3af4718b2b2b52d07
Andrew Hinojosa <drews_view@hotmail.com>
2020-06-29 04:24:19 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   i = 0 
   collection = []
   while array.length < 0
-    collection << yield(array[i])
+    collection << yield(array[i].split(" ").first)
   end
   collection
 end
